01e24da3c21f3957f15b47e533b99009780eccee
HirushaniGamage <chanukshi@gmail.com>
2022-05-05 01:24:31 +0530

    frontend navigation partially implemented

---
+++ Frontend/src/app/commoncomponents/ui/navigation.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Se</h1>
+      <ul>
+        <li>Threader</li>
+        <li>Customer</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
